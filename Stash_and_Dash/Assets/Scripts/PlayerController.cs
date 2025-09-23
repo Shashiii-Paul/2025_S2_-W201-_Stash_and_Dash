25ea6d2ec238b46cce0228d4ba9a335f13b16765
baba2555 <haifa.pahore@gmail.com>
2025-09-23 17:09:32 +1200

    First-Person-View

--- Stash_and_Dash/Assets/Scripts/PlayerController.cs
+++ Stash_and_Dash/Assets/Scripts/PlayerController.cs
@@ -3,31 +3,49 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] Animator animator;
-    [SerializeField] float moveSpeed;
+    [SerializeField] float moveSpeed = 5f; 
     [SerializeField] Rigidbody rb;
-    Vector3 _moveInput;
+    [SerializeField] Transform cameraTransform; 
+    [SerializeField] float mouseSensitivity = 100f; 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector3 _moveInput;
+    private float xRotation = 0f; 
+
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked; 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
         _moveInput = new Vector3(h, 0f, v).normalized;
+
+        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); 
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        
+        transform.Rotate(Vector3.up * mouseX);
     }
 
     private void FixedUpdate()
     {
-        Vector3 moveVelocity = _moveInput * moveSpeed;
-        Vector3 newPosition = rb.position + moveVelocity * Time.deltaTime;
+        
+        Vector3 moveDirection = (transform.forward * _moveInput.z + transform.right * _moveInput.x).normalized;
+        Vector3 moveVelocity = moveDirection * moveSpeed;
+        Vector3 newPosition = rb.position + moveVelocity * Time.fixedDeltaTime;
         rb.MovePosition(newPosition);
-        animator.SetFloat("MoveX", _moveInput.x);
-        animator.SetFloat("MoveZ", _moveInput.z); 
-    }
 
+        
+        animator.SetFloat("MoveX", _moveInput.x);
+        animator.SetFloat("MoveZ", _moveInput.z);
+    }
 }
