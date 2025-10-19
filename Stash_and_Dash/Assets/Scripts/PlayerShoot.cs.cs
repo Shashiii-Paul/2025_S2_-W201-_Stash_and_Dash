e5905a891d43d988442c1bc53988bf7e6f414684
baba2555 <haifa.pahore@gmail.com>
2025-10-19 21:34:31 +1300

    Health-Decrease
    Haifa and Kirby

--- Stash_and_Dash/Assets/Scripts/PlayerShoot.cs.cs
+++ Stash_and_Dash/Assets/Scripts/PlayerShoot.cs.cs
@@ -15,8 +15,18 @@ public class PlayerShoot : AttributesSync
 
     private void Start()
     {
-        if (avatar.IsMe)
-            avatar.gameObject.layer = playerSelfLayer;
+        // Set layer recursively on root (avatar.gameObject) and all children
+        int targetLayer = avatar.IsMe ? playerSelfLayer : LayerMask.NameToLayer("Player");
+        SetLayerRecursively(avatar.gameObject, targetLayer);
+    }
+
+    private void SetLayerRecursively(GameObject obj, int newLayer)
+    {
+        obj.layer = newLayer;
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
     }
 
     private void Update()
@@ -30,18 +40,33 @@ public class PlayerShoot : AttributesSync
 
     void Shoot()
     {
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit,
-                Mathf.Infinity, playerLayer))
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, Mathf.Infinity, playerLayer))
         {
-            PlayerShoot playerShoot = hit.transform.GetComponentInChildren<PlayerShoot>();
-            playerShoot.Hit(damage);
+            Debug.Log("Raycast hit: " + hit.transform.name);  // Check if raycast detects anything
+            // Get root (Player), then find PlayerShoot script on its children
+            PlayerShoot playerShoot = hit.transform.root.GetComponentInChildren<PlayerShoot>();
+            if (playerShoot != null)
+            {
+                Debug.Log("Found PlayerShoot on: " + playerShoot.gameObject.name + " | IsMe: " + playerShoot.avatar.IsMe);
+                // Call remotely for network sync instead of direct
+                playerShoot.BroadcastRemoteMethod(nameof(Hit), damage);
+            }
+            else
+            {
+                Debug.Log("No PlayerShoot component found on hit object");
+            }
+        }
+        else
+        {
+            Debug.Log("No raycast hit");
         }
     }
 
+    [SynchronizableMethod]  // Makes this callable over network
     public void Hit(int damageTaken)
     {
+        Debug.Log("Hit called on: " + gameObject.name + " | Current health: " + health + " | Damage: " + damageTaken + " | IsMe: " + avatar.IsMe);
         health -= damageTaken;
-
         if (health <= 0)
         {
             BroadcastRemoteMethod("Die");
